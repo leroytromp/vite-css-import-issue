73f518fdf74e59683e72cc81edb70400cdeba778
Leroy Tromp <l.a.j.tromp@gmail.com>
2021-06-24 19:22:57 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+  import '@patternfly/patternfly/patternfly.min.css';
+</script>
+
+<main>
+  <h1 class="pf-c-title pf-m-4xl">Welcome to Vite</h1>
+
+  <button class="pf-c-button pf-m-primary" type="button">Primary</button>
+
+  <button class="pf-c-button pf-m-control" type="button" aria-label="Copy input">
+    <i class="fas fa-copy" aria-hidden="true" />
+  </button>
+
+  <i class="fas fa-bug" />
+
+  <p>
+    The squares are supposed to be icons, and the font is not correct. Downgrading to vite 2.3.7
+    solves the issue and loads the icons and fonts.
+  </p>  
+</main>
